4f7f81432897c10c5312be44925c63558afddfc2
James <james333james.ct@gmail.com>
2019-09-15 09:55:25 +0700

    Morning update

---
+++ react-101/src/Counter.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Counter = (props) => {
+    const [timer, setTimer] = React.useState(0);
+    // const [state2, setState2] = React.useState('')
+    React.useEffect(() => {
+        console.log('in use effect')
+    })
+    const handleClick = () => {
+        setTimer(timer + 1)
+    }
+    return (
+        <div>
+            Timer: {timer}
+            <button type="button" onClick={handleClick}>Add</button>
+        </div>
+    )
+}
+
+// class Counter extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             timer: 0,
+//         }
+//     }
+//     componentDidMount() {
+//         this.interval = setInterval(() => {
+//             this.setState(state => ({ timer: state.timer + 1}))
+//         })
+//     }
+// }
+export default Counter
